3b070abe516f836557c241a478be036c1d34587c
Chris Dickinson <christopher.s.dickinson@gmail.com>
2016-01-07 18:06:11 -0800

    add yet more tests

---
+++ test/basic-atomic-concurrency-test.js
@@ -0,0 +1,134 @@
+'use strict'
+
+const Promise = require('bluebird')
+const domain = require('domain')
+const test = require('tap').test
+
+const db = require('../db-session.js')
+
+const LOGS = []
+
+const runOperations = db.transaction(function runOperations (inner) {
+  return Promise.all(Array.from(Array(4)).map((_, idx) => {
+    return idx % 2 === 0 ? inner(idx) : db.getConnection().then(connPair => {
+      LOGS.push(`load ${idx}`)
+      return Promise.delay(5).then(() => {
+        LOGS.push(`release ${idx}`)
+        connPair.release()
+      })
+    })
+  }))
+})
+
+function runSubOperation (rootIdx) {
+  return Promise.all(Array.from(Array(4)).map((_, idx) => {
+    return Promise.delay(5).then(() => {
+      return db.getConnection().then(connPair => {
+        LOGS.push(`load ${rootIdx} ${idx}`)
+        return Promise.delay(5).then(() => {
+          LOGS.push(`release ${rootIdx} ${idx}`)
+          connPair.release()
+        })
+      })
+    })
+  }))
+}
+
+const txRunSubOperation = db.transaction(runSubOperation)
+const atomicRunSubOperation = db.atomic(runSubOperation)
+
+test('test nested transaction order', assert => {
+  LOGS.length = 0
+  const domain1 = domain.create()
+  db.install(domain1, innerGetConnection, {maxConcurrency: 0})
+  domain1.run(() => {
+    return runOperations(txRunSubOperation)
+  }).then(() => {
+    assert.equal(process.domain, undefined)
+    domain1.exit()
+  }).then(() => {
+    assert.equal(LOGS.join('\n'), `
+BEGIN
+load 1
+release 1
+load 3
+release 3
+load 0 0
+release 0 0
+load 0 1
+release 0 1
+load 0 2
+release 0 2
+load 0 3
+release 0 3
+load 2 0
+release 2 0
+load 2 1
+release 2 1
+load 2 2
+release 2 2
+load 2 3
+release 2 3
+COMMIT
+release
+`.trim())
+    assert.equal(process.domain, undefined)
+  }).then(() => assert.end())
+    .catch(assert.end)
+})
+
+test('test nested atomic transaction order', assert => {
+  LOGS.length = 0
+  const domain1 = domain.create()
+  db.install(domain1, innerGetConnection, {maxConcurrency: 0})
+  domain1.run(() => {
+    return runOperations(atomicRunSubOperation)
+  }).then(() => {
+    assert.equal(process.domain, undefined)
+    domain1.exit()
+  }).then(() => {
+    assert.equal(LOGS.join('\n').replace(/_[\d_]+$/gm, '_TS'), `
+BEGIN
+SAVEPOINT save_0_runSubOperation_TS
+load 0 0
+release 0 0
+load 0 1
+release 0 1
+load 0 2
+release 0 2
+load 0 3
+release 0 3
+RELEASE SAVEPOINT save_0_runSubOperation_TS
+load 1
+release 1
+SAVEPOINT save_1_runSubOperation_TS
+load 2 0
+release 2 0
+load 2 1
+release 2 1
+load 2 2
+release 2 2
+load 2 3
+release 2 3
+RELEASE SAVEPOINT save_1_runSubOperation_TS
+load 3
+release 3
+COMMIT
+release
+`.trim())
+    assert.equal(process.domain, undefined)
+  }).then(() => assert.end())
+    .catch(assert.end)
+})
+
+function innerGetConnection () {
+  return {
+    connection: {query (sql, ready) {
+      LOGS.push(sql)
+      return ready()
+    }},
+    release () {
+      LOGS.push(`release`)
+    }
+  }
+}
---
+++ test/basic-transaction-concurrency-test.js
@@ -0,0 +1,71 @@
+'use strict'
+
+const Promise = require('bluebird')
+const domain = require('domain')
+const test = require('tap').test
+
+const db = require('../db-session.js')
+
+const LOGS = []
+
+const runOperations = db.transaction(function runOperations () {
+  return Promise.all(Array.from(Array(8)).map((_, idx) => {
+    return db.getConnection().then(connPair => {
+      LOGS.push(`load ${idx}`)
+      return Promise.delay(5).then(() => {
+        LOGS.push(`release ${idx}`)
+        connPair.release()
+      })
+    })
+  }))
+})
+
+test('test root session concurrency=0', assert => {
+  // compare to ./basic-session-concurrency-test.js, concurrency=1 —
+  // operations should load and release in sequence, and be bookended
+  // by "BEGIN" / "END"
+  const domain1 = domain.create()
+  db.install(domain1, innerGetConnection, {maxConcurrency: 0})
+  domain1.run(() => {
+    return runOperations()
+  }).then(() => {
+    assert.equal(process.domain, undefined)
+    domain1.exit()
+  }).then(() => {
+    assert.equal(LOGS.join('\n'), `
+BEGIN
+load 0
+release 0
+load 1
+release 1
+load 2
+release 2
+load 3
+release 3
+load 4
+release 4
+load 5
+release 5
+load 6
+release 6
+load 7
+release 7
+COMMIT
+release
+`.trim())
+    assert.equal(process.domain, undefined)
+  }).then(() => assert.end())
+    .catch(assert.end)
+})
+
+function innerGetConnection () {
+  return {
+    connection: {query (sql, ready) {
+      LOGS.push(sql)
+      return ready()
+    }},
+    release () {
+      LOGS.push(`release`)
+    }
+  }
+}
