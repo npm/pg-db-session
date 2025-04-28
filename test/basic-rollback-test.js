499ad4a679f355c97e904009d9ceaadcb871c89e
jpg619 <jpg619@github.com>
2025-04-28 13:43:13 -0400

    comment 3.1

--- test/basic-rollback-test.js
+++ test/basic-rollback-test.js
@@ -16,7 +16,7 @@ test('rolling back transaction calls ROLLBACK', assert => {
   domain1.run(() => {
     return db.transaction(() => {
       throw new Error('no thanks')
-    })().catch(() => undefined)
+    })().catch(() => {})
   })
   .then(() => assert.equal(LOGS.join(' '), 'BEGIN ROLLBACK'))
   .catch(err => assert.fail(err))
@@ -44,7 +44,7 @@ test('rolling back atomic calls ROLLBACK', assert => {
   domain1.run(() => {
     return db.atomic(() => {
       throw new Error('no thanks')
-    })().catch(() => undefined)
+    })().catch(() => {})
   })
   .then(() => {
     assert.equal(LOGS.join('\n').replace(/_[\d_]+$/gm, '_TS'), `
